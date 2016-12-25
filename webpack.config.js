fe882feb661c01b343d016fd2165f8154589bbc8
huozhi <inbox@huozhi.im>
2016-12-25 18:53:08 +0800

    init reactip with demo

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+const joinPath = path.join.bind(null, __dirname)
+
+module.exports = {
+  entry: {
+    index: [joinPath('src/index.js')],
+  },
+  output: {
+    publicPath: '/',
+    path: joinPath('lib'),
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css!postcss',
+      },
+    ],
+    resolve: {
+      alias: {
+        'react': path.resolve(__dirname, 'node_modules/react'),
+      }
+    }
+  },
+  externals: {
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    }
+  },
+  postcss: webpack => [
+    require('postcss-nested'),
+  ],
+}
