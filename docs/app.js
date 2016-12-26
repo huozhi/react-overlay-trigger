dfeb53ce314211815e10405258a1c97b8b68c044
Jiachi Liu <inbox@huozhi.im>
2016-12-26 11:51:21 +0800

    handle ref for stateful react component

--- docs/app.js
+++ docs/app.js
@@ -3,6 +3,13 @@ import {render} from 'react-dom'
 import Tooltip from 'reactip'
 import './app.css'
 
+class Button extends React.Component {
+  render() {
+    const {children, ...rest} = this.props
+    return <button {...rest}>{children}</button>
+  }
+}
+
 const App = () => {
   return (
     <div className="App">
@@ -13,7 +20,7 @@ const App = () => {
 
       <div className="Demo">
         <Tooltip position="right" tooltip="something">
-          <button className="Toggler">right</button>
+          <Button className="Toggler">right</Button>
         </Tooltip>
 
         <Tooltip position="top" tooltip="something">
