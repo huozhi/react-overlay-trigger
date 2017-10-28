3e9ba3d882bf07bff22338c126fbf4ee96033c1a
huozhi <gilesliu12@gmail.com>
2017-10-28 11:34:24 +0800

    bundle with rollup

--- src/Arrow.js
+++ src/Arrow.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {css} from 'emotion'
-import styled from 'react-emotion'
 import {isHorizontal, getOppositePlacement} from './utils'
 
 const Arrow = ({placement, arrowColor = '#fff', arrowSize = 5}) => (
