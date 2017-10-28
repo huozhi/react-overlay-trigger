2a6f12dc38fcb0c301d21c6def38b23594a569d2
huozhi <gilesliu12@gmail.com>
2017-10-28 10:23:31 +0800

    handle arrow size

--- src/Popup.js
+++ src/Popup.js
@@ -16,16 +16,13 @@ class Popup extends React.Component {
   }
 
   componentDidMount() {
-    const {placement, target, onRef} = this.props
+    const {placement, target, arrowSize} = this.props
     // TODO: default bcr
-    // console.log('this.ref', this.ref)
-    let rect = position(placement, this.ref, target)
+    let rect = position(placement, this.ref, target, arrowSize)
     let finalPlacement = placement
-    console.log('origin rect', rect)
     if (!isInViewport(rect)) {
       finalPlacement = getOppositePlacement(placement)
-      rect = position(finalPlacement, this.ref, target)
-      console.log('getOppositePlacement rect', rect)
+      rect = position(finalPlacement, this.ref, target, arrowSize)
     }
     this.setState({style: rect, placement: finalPlacement})
   }
