42c976380a496ee16b6d154f18663041391abf9e
huozhi <inbox@huozhi.im>
2019-09-29 16:22:57 +0800

    feat: overlay (#12)
    * feat: overlay
    
    * update docs and pkg.json

--- docs/app.js
+++ docs/app.js
@@ -1,61 +1,83 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Tooltip from 'reactip'
+import OverlayTrigger from 'react-overlay-trigger'
 import './app.css'
 
-const Button = ({children, ...rest}) => (
-  <button {...rest}>{children}</button>
-)
+const usageDescription = `
+function Overlay({style, ...rest}) {
+  return <span style={{...style, padding: 10, backgroundColor: 'rgba(255, 255, 255, .3)'}}>tooltip</span>
+}
+
+const overlay = <Overlay>tooltip</Overlay>
+
+<OverlayTrigger placement="left" trigger="hover" overlay={overlay}>
+  <button className="Toggler">hover [left]</button>
+</OverlayTrigger>
+
+<OverlayTrigger placement="right" trigger="hover" overlay={overlay}>
+  <button className="Toggler">hover [right]</button>
+</OverlayTrigger>
+
+<OverlayTrigger placement="top" trigger="click" overlay={overlay}>
+  <button className="Toggler">click [top]</button>
+</OverlayTrigger>
+
+<OverlayTrigger placement="bottom" trigger="click" overlay={overlay}>
+  <button className="Toggler">click [bottom]</button>
+</OverlayTrigger>
+`
+
+function Overlay({style, ...rest}) {
+  return (
+    <span
+      style={{
+        ...style,
+        padding: '2px 8px',
+        backgroundColor: 'rgba(255, 255, 255, .05)'
+      }}
+    >
+      tooltip
+    </span>
+  )
+}
 
 const App = () => {
+  const overlay = <Overlay>yep</Overlay>
   return (
     <div className="App">
       <div className="App-titile">
-        REACTIP
+        React Overlay Trigger
         <small style={{fontSize: 22, marginLeft: 30}}>Easy Tooltip Component for React</small>
         <div className="App-github">
-          <iframe src={`https://ghbtns.com/github-btn.html?user=huozhi&repo=reactip&type=star`} frameBorder="0" scrolling="0" width="60px" height="20px" />
+          <iframe src={`https://ghbtns.com/github-btn.html?user=huozhi&repo=react-overlay-trigger&type=star`} frameBorder="0" scrolling="0" width="60px" height="20px" />
         </div>
       </div>
       <p className="App-subtitle">Examples</p>
 
       <div className="Demo">
-        <Tooltip placement="left" event="hover" tooltip="something">
-          <button className="Toggler">hover [left]</button>
-        </Tooltip>
+        <div className="Demo-item">
+          <OverlayTrigger placement="left" trigger="hover" overlay={overlay}>
+            <button className="Toggler">hover [left]</button>
+          </OverlayTrigger>
 
-        <Tooltip placement="right" event="hover" tooltip="something">
-          <Button className="Toggler">hover [right]</Button>
-        </Tooltip>
+          <OverlayTrigger placement="right" trigger="hover" overlay={overlay}>
+            <button className="Toggler">hover [right]</button>
+          </OverlayTrigger>
+        </div>
+        <div className="Demo-item">
+          <OverlayTrigger placement="top" trigger="click" overlay={overlay}>
+            <button className="Toggler">click [top]</button>
+          </OverlayTrigger>
 
-        <Tooltip placement="top" event="click" tooltip="something">
-          <button className="Toggler">click [top]</button>
-        </Tooltip>
-
-        <Tooltip placement="bottom" event="click" tooltip="something">
-          <button className="Toggler">click [bottom]</button>
-        </Tooltip>
+          <OverlayTrigger placement="bottom" trigger="click" overlay={overlay}>
+            <button className="Toggler">click [bottom]</button>
+          </OverlayTrigger>
+        </div>
       </div>
 
       <pre>
         <code className="App-code language-javascript">
-        {`
-          <Tooltip placement="left" event="hover" tooltip="something">
-            <button className="Toggler">hover [left]</button>
-          </Tooltip>
-
-          <Tooltip placement="right" event="hover" tooltip="something">
-            <Button className="Toggler">hover [right]</Button>
-          </Tooltip>
-
-          <Tooltip placement="top" event="click" tooltip="something">
-            <button className="Toggler">click [top]</button>
-          </Tooltip>
-
-          <Tooltip placement="bottom" event="click" tooltip="something">
-            <button className="Toggler">click [bottom]</button>
-          </Tooltip>
-        `}
+          {usageDescription}
         </code>
       </pre>
     </div>
---
+++ src/OverlayTrigger.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Overlay from './Overlay'
+
+class RefHolder extends React.Component {
+  render() {
+    return this.props.children
+  }
+}
+
+class OverlayTrigger extends React.Component {
+  static defaultProps = {
+    container: document ? document.body : null,
+    trigger: 'hover',
+    placement: 'left',
+  }
+
+  constructor(props) {
+    super(props)
+    this.triggerRef = React.createRef()
+    this.state = {
+      visible: false,
+    }
+  }
+
+  safeCall = (fn) => {
+    if (typeof fn === 'function') {
+      fn()
+    }
+  }
+
+  getChildProps() {
+    return this.props.children.props
+  }
+
+  handleMouseEnter = () => {
+    this.safeCall(this.getChildProps().onMouseEnter)
+    this.open()
+  }
+
+  handleMouseLeave = () => {
+    this.safeCall(this.getChildProps().onMouseLeave)
+    this.close()
+  }
+
+  handleClick = () => {
+    this.safeCall(this.getChildProps().onClick)
+    if (this.state.visible) {
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+
+  getTarget = () => {
+    return ReactDOM.findDOMNode(this.triggerRef.current)
+  }
+
+  getTriggerProps = () => {
+    switch (this.props.trigger) {
+      case 'hover':
+        return {
+          onMouseEnter: this.handleMouseEnter,
+          onMouseLeave: this.handleMouseLeave,
+        }
+      case 'click':
+        return {
+          onClick: this.handleClick,
+        }
+      default:
+        return {}
+    }
+  }
+
+  open = () => {
+    this.setState({visible: true})
+  }
+
+  close = () => {
+    this.setState({visible: false})
+  }
+
+  render() {
+    const {children, container, overlay, arrowSize, placement} = this.props
+    const child = React.Children.only(children)
+    return (
+      <React.Fragment>
+        <RefHolder ref={this.triggerRef}>
+          {(child != null && child !== false) && React.cloneElement(child, this.getTriggerProps())}
+        </RefHolder>
+        {this.state.visible &&
+          <Overlay
+            arrowSize={arrowSize}
+            container={container}
+            placement={placement}
+            target={this.getTarget}
+          >
+            {overlay}
+          </Overlay>
+        }
+      </React.Fragment>
+    )
+  }
+}
+
+export default OverlayTrigger
