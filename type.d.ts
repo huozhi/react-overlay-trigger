2cbed944d608fd2ad59c14dfb0046352e65e4809
Jiachi Liu <inbox@huozhi.im>
2023-06-25 22:51:45 +0200

    fix type

--- type.d.ts
+++ type.d.ts
@@ -1,16 +1,16 @@
-import * as React from 'react';
+import * as React from 'react'
 
-type TriggerType = 'hover' | 'click' | 'focus';
-type PlacementType = 'top' | 'bottom' | 'left' | 'right' | 'center';
+type TriggerType = 'hover' | 'click' | 'focus'
+type PlacementType = 'top' | 'bottom' | 'left' | 'right' | 'center'
 
 export interface OverlayTriggerProps {
-  overlay: React.ReactNode;
-  children: React.ReactNode;
-  triggers: TriggerType[];
-  container?: HTMLElement;
-  placement: PlacementType;
+  overlay: React.ReactNode
+  children: React.ReactNode
+  triggers: TriggerType[]
+  container?: HTMLElement
+  placement: PlacementType
 }
 
-declare const OverlayTrigger: React.ClassicComponentClass<OverlayTriggerProps>;
+declare const OverlayTrigger: React.ClassicComponentClass<OverlayTriggerProps>
 
-export default OverlayTrigger;
+export default OverlayTrigger
